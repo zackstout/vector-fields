99436a2a16833a70b74de7dddbcec187f7997e25
Zack Stout <zackstout@gmail.com>
2018-02-22 21:28:12 -0600

    this makes a cool sunshine-like image

--- draw.js
+++ draw.js
@@ -1,24 +1,40 @@
 
 var gridPoints = [];
+var s = 20;
+var w;
 
 function setup () {
-  var size = 20;
   createCanvas(600, 600);
   background(100);
-  console.log(width/size);
-  for (var i=0; i < width/size; i++) {
-    for (var j=0; j < width/size; j++) {
-      var pointY = j * size;
-      var pointX = i * size;
+  // console.log(width/size);
+  w = width;
+  // s = 20;
+  makeGrid();
+
+  addVectors();
+
+  console.log(gridPoints);
+}
+
+function addVectors() {
+  gridPoints.forEach(function(pt) {
+    var xDis = (w/2 - pt.x) / 450;
+    var yDis = (w/2 - pt.y) / 450;
+    stroke(255);
+    line(pt.x, pt.y, xDis, yDis);
+  });
+}
+
+function makeGrid() {
+  for (var i=0; i < w/s; i++) {
+    for (var j=0; j < w/s; j++) {
       var point = {
-        x: pointX,
-        y: pointY
+        x: j * s,
+        y: i * s
       };
       gridPoints.push(point);
     }
   }
-
-  console.log(gridPoints);
 }
 
 
